74a44d205acce2b02226758c53830566d4128a83
suetAndTie <trtets@gmail.com>
2019-02-11 22:52:35 -0800

    chord only lstm

--- Backend/Model/model/loss.py
+++ Backend/Model/model/loss.py
@@ -31,28 +31,28 @@ def midi_loss(output, target, extra=None):
 
     return F.nll_loss(flat_melody_out, flat_melody_y) + F.multilabel_soft_margin_loss(flat_chord_out, flat_chord_y)
 
-    def midi_loss_chord_only(output, target, extra=None):
-        melody_out = output['melody_out']
-        chord_out = output['chord_out']
-        melody_y = target['melody_y']
-        chord_y = target['chord_y']
+def midi_loss_chord_only(output, target, extra=None):
+    melody_out = output['melody_out']
+    chord_out = output['chord_out']
+    melody_y = target['melody_y']
+    chord_y = target['chord_y']
 
-        batch_size, seq_len, vocab_size = melody_out.shape
-        seq_lengths = extra['seq_lengths']
-        device = melody_out.device
+    batch_size, seq_len, vocab_size = melody_out.shape
+    seq_lengths = extra['seq_lengths']
+    device = melody_out.device
 
-        # flatten (batchsize, seq_len, vocab_size) -> (batchsize * seq_len, vocab_size)
-        # contiguous returns tensor with same data
-        flat_chord_out = torch.tensor([]).to(device)
-        flat_chord_y = torch.tensor([]).to(device)
-        for i in range(batch_size):
-            flat_chord_out = torch.cat((flat_chord_out, chord_out[i, :seq_lengths[i]]))
-            flat_chord_y = torch.cat((flat_chord_y, chord_y[i, :seq_lengths[i]].float()))
+    # flatten (batchsize, seq_len, vocab_size) -> (batchsize * seq_len, vocab_size)
+    # contiguous returns tensor with same data
+    flat_chord_out = torch.tensor([]).to(device)
+    flat_chord_y = torch.tensor([]).to(device)
+    for i in range(batch_size):
+        flat_chord_out = torch.cat((flat_chord_out, chord_out[i, :seq_lengths[i]]))
+        flat_chord_y = torch.cat((flat_chord_y, chord_y[i, :seq_lengths[i]].float()))
 
-        # melody_out = melody_out[:, seq_len-1].contiguous().view(-1, vocab_size)
-        # chord_out = chord_out[:, seq_len-1].contiguous().view(-1, vocab_size)
-        # melody_y = melody_y[:, seq_len-1].flatten()
-        # chord_y = chord_y[:, seq_len-1].view(-1, vocab_size)
+    # melody_out = melody_out[:, seq_len-1].contiguous().view(-1, vocab_size)
+    # chord_out = chord_out[:, seq_len-1].contiguous().view(-1, vocab_size)
+    # melody_y = melody_y[:, seq_len-1].flatten()
+    # chord_y = chord_y[:, seq_len-1].view(-1, vocab_size)
 
 
-        return F.multilabel_soft_margin_loss(flat_chord_out, flat_chord_y)
+    return F.multilabel_soft_margin_loss(flat_chord_out, flat_chord_y)
