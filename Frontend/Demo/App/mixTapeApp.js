d686c324575ee0c9c7c4ff2fce1523ed622596f4
adm116 <adm116@stanford.edu>
2019-02-13 16:08:41 -0800

    preload notes

--- Frontend/Demo/App/mixTapeApp.js
+++ Frontend/Demo/App/mixTapeApp.js
@@ -17,14 +17,39 @@ angular.module("mixTapeApp", [])
         $scope.chordDurations = [];
         $scope.topMessage = "Welcome to Mixtape!";
 
+    sounds = {
+        'A3' : new Howl({ src: ['App/aud/A3.wav'], volume: 0.4}),
+        'A4' : new Howl({ src: ['App/aud/A4.wav'], volume: 0.4}),
+        'B-3' : new Howl({ src: ['App/aud/B-3.wav'], volume: 0.4}),
+        'B-4' : new Howl({ src: ['App/aud/B-4.wav'], volume: 0.4}),
+        'B3' : new Howl({ src: ['App/aud/B3.wav'], volume: 0.4}),
+        'B4' : new Howl({ src: ['App/aud/B4.wav'], volume: 0.4}),
+        'C3' : new Howl({ src: ['App/aud/C3.wav'], volume: 0.4}),
+        'C4' : new Howl({ src: ['App/aud/C4.wav'], volume: 0.4}),
+        'CS3' : new Howl({ src: ['App/aud/CS3.wav'], volume: 0.4}),
+        'CS4' : new Howl({ src: ['App/aud/CS4.wav'], volume: 0.4}),
+        'D3' : new Howl({ src: ['App/aud/D3.wav'], volume: 0.4}),
+        'D4' : new Howl({ src: ['App/aud/D4.wav'], volume: 0.4}),
+        'E-3' : new Howl({ src: ['App/aud/E-3.wav'], volume: 0.4}),
+        'E-4' : new Howl({ src: ['App/aud/E-4.wav'], volume: 0.4}),
+        'E3' : new Howl({ src: ['App/aud/E3.wav'], volume: 0.4}),
+        'E4' : new Howl({ src: ['App/aud/E4.wav'], volume: 0.4}),
+        'F3' : new Howl({ src: ['App/aud/F3.wav'], volume: 0.4}),
+        'F4' : new Howl({ src: ['App/aud/F4.wav'], volume: 0.4}),
+        'FS3' : new Howl({ src: ['App/aud/FS3.wav'], volume: 0.4}),
+        'FS4' : new Howl({ src: ['App/aud/FS4.wav'], volume: 0.4}),
+        'G3' : new Howl({ src: ['App/aud/G3.wav'], volume: 0.4}),
+        'G4' : new Howl({ src: ['App/aud/G4.wav'], volume: 0.4}),
+        'GS3' : new Howl({ src: ['App/aud/GS3.wav'], volume: 0.4}),
+        'GS4' : new Howl({ src: ['App/aud/GS4.wav'], volume: 0.4}),
+    };
+
     getMelody = function() {
         var melody = utilsService.getMelody();
         $scope.melodySounds = [];
         for (var i = 0; i < melody.length; i++) {
             var note = utilsService.cleanNote(melody[i]);
-            var file = 'App/aud/' + note + '.wav';
-            var howl = new Howl({ src: [file], volume: 0.4});
-            $scope.melodySounds.push(howl);
+            $scope.melodySounds.push(sounds[note]);
         }
 
         $scope.melody = melody;
@@ -43,9 +68,7 @@ angular.module("mixTapeApp", [])
             var chordObj = [];
             for (var j = 0; j < splitChord.length; j++) {
                 var note = utilsService.cleanNote(splitChord[j] + '4');
-                var file = 'App/aud/' + note + '.wav';
-                var howl = new Howl({ src: [file], volume: 0.4});
-                chordObj.push(howl);
+                chordObj.push(sounds[note]);
             }
 
             $scope.chordSounds.push(chordObj);
@@ -69,7 +92,7 @@ angular.module("mixTapeApp", [])
 
         utilsService.playSequence($scope.chordSounds, $scope.chordDurations, true);
     };
-    
+
 
     $scope.playComplete = async function() {
         getMelody();
@@ -83,11 +106,11 @@ angular.module("mixTapeApp", [])
     $scope.updateNoteType = function(){
       globalSettings.currentType = $scope.currentType;
     };
-    
+
     $scope.updatePitchType = function(){
-      globalSettings.pitchType = $scope.pitchType;  
+      globalSettings.pitchType = $scope.pitchType;
     };
-    
+
 }])
 
     .directive("mixtapeApp", ["$interval", "renderService", "graphicsEngineService", "utilsService", "globalSettings",
